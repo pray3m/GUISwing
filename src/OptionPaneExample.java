445f705c2482c343397f160b0f6b44d2cc369235
pray3m <prem.gtm9@gmail.com>
2024-07-04 23:32:38 +0545

    List, OptionPane and Scroll bar in swing

---
+++ src/OptionPaneExample.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class OptionPaneExample {
+    OptionPaneExample(){
+       JFrame f=new JFrame();
+        JOptionPane.showMessageDialog(f,"Hello, Welcome to Java GUI.");
+    }
+    public static void main(String[] args) {
+        new OptionPaneExample();
+    }
+}  
